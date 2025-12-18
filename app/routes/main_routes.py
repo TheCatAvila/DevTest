cbc9d8b7e6ea1b5981318c928e2f52badd51fecd
TheCatAvila <thecatavila@gmail.com>
2025-12-18 02:27:21 -0300

    Update: cambios antes del despliegue

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -24,3 +24,12 @@ def daily_tech():
     user_winner = session.get('user_winner', False)
 
     return render_template('daily_tech.html', guessed_techs_data=guessed_techs_data, attempts=attempts, user_winner=user_winner)
+
+@main.route('/free_tech')
+def free_tech():
+
+    guessed_techs_data = session.get('guessed_techs', [])
+    attempts = session.get('attempts', 0)
+    user_winner = session.get('user_winner', False)
+
+    return render_template('free_tech.html', guessed_techs_data=guessed_techs_data, attempts=attempts, user_winner=user_winner)
